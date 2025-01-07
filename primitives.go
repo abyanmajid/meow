52b36584d1a5b34b4b46fe988e17761a8be31f3f
abyanmajid <abyan@abydyl.net>
2025-01-08 00:07:32 +0700

    chore: add comment docs for primitives

--- primitives.go
+++ primitives.go
@@ -6,38 +6,47 @@ import (
 	"time"
 )
 
+// MeowStringSchema represents a schema for validating string inputs.
 type MeowStringSchema struct {
 	MeowBaseSchema[string]
 }
 
+// MeowNumberSchema represents a schema for validating number inputs (float64).
 type MeowNumberSchema struct {
 	MeowBaseSchema[float64]
 }
 
+// MeowBooleanSchema represents a schema for validating boolean inputs.
 type MeowBooleanSchema struct {
 	MeowBaseSchema[bool]
 }
 
+// MeowDateSchema represents a schema for validating date inputs (time.Time).
 type MeowDateSchema struct {
 	MeowBaseSchema[time.Time]
 }
 
+// MeowNullSchema represents a schema for validating nil inputs.
 type MeowNullSchema struct {
 	MeowBaseSchema[interface{}]
 }
 
+// MeowAnySchema represents a schema that validates any input without restrictions.
 type MeowAnySchema struct {
 	MeowBaseSchema[interface{}]
 }
 
+// MeowNeverSchema represents a schema that always fails validation.
 type MeowNeverSchema struct {
 	MeowBaseSchema[interface{}]
 }
 
+// String creates a schema for validating string inputs.
 func String() *MeowStringSchema {
 	return &MeowStringSchema{
 		MeowBaseSchema[string]{
 			validateFunc: func(input string) error {
+				// Check if the input is a string
 				if reflect.TypeOf(input).Kind() != reflect.String {
 					return errors.New("input is not a string")
 				}
@@ -47,10 +56,12 @@ func String() *MeowStringSchema {
 	}
 }
 
+// Number creates a schema for validating number inputs (float64).
 func Number() *MeowNumberSchema {
 	return &MeowNumberSchema{
 		MeowBaseSchema[float64]{
 			validateFunc: func(input float64) error {
+				// Check if the input is a float64
 				if reflect.TypeOf(input).Kind() != reflect.Float64 {
 					return errors.New("input is not a number")
 				}
@@ -60,10 +71,12 @@ func Number() *MeowNumberSchema {
 	}
 }
 
+// Boolean creates a schema for validating boolean inputs.
 func Boolean() *MeowBooleanSchema {
 	return &MeowBooleanSchema{
 		MeowBaseSchema[bool]{
 			validateFunc: func(input bool) error {
+				// Check if the input is a boolean
 				if reflect.TypeOf(input).Kind() != reflect.Bool {
 					return errors.New("input is not a boolean")
 				}
@@ -73,20 +86,24 @@ func Boolean() *MeowBooleanSchema {
 	}
 }
 
+// Date creates a schema for validating date inputs (time.Time).
 func Date() *MeowDateSchema {
 	return &MeowDateSchema{
 		MeowBaseSchema[time.Time]{
 			validateFunc: func(input time.Time) error {
+				// Since the input is already a time.Time, this is always valid
 				return nil
 			},
 		},
 	}
 }
 
+// Nil creates a schema for validating nil inputs.
 func Nil() *MeowNullSchema {
 	return &MeowNullSchema{
 		MeowBaseSchema[interface{}]{
 			validateFunc: func(input interface{}) error {
+				// Check if the input is nil
 				if input != nil {
 					return errors.New("input is neither nil")
 				}
@@ -96,20 +113,24 @@ func Nil() *MeowNullSchema {
 	}
 }
 
+// Any creates a schema that validates any input without restrictions.
 func Any() *MeowAnySchema {
 	return &MeowAnySchema{
 		MeowBaseSchema[interface{}]{
 			validateFunc: func(input interface{}) error {
+				// Always valid
 				return nil
 			},
 		},
 	}
 }
 
+// Never creates a schema that always fails validation.
 func Never() *MeowNeverSchema {
 	return &MeowNeverSchema{
 		MeowBaseSchema[interface{}]{
 			validateFunc: func(input interface{}) error {
+				// Always fails validation
 				return errors.New("input is never valid")
 			},
 		},
