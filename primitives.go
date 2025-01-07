e1fa9936090079e0b422642626472b809bf4d21a
abyanmajid <abyan@abydyl.net>
2025-01-08 01:22:54 +0700

    chore: test cov 75% str and num primitives, also include gitignore

--- primitives.go
+++ primitives.go
@@ -3,6 +3,7 @@ package meow
 import (
 	"errors"
 	"fmt"
+	"math"
 	"reflect"
 )
 
@@ -14,7 +15,7 @@ func String(varName string) *MeowSchema {
 	return &MeowSchema{
 		Parse: func(input any) error {
 			if input == nil || reflect.TypeOf(input).Kind() != reflect.String {
-				errMsg := fmt.Sprintf("'%s' is not a string.", varName)
+				errMsg := fmt.Sprintf("[%s]: '%s' is not a string.", varName, input)
 				return errors.New(errMsg)
 			}
 			return nil
@@ -25,10 +26,29 @@ func String(varName string) *MeowSchema {
 func Number(varName string) *MeowSchema {
 	return &MeowSchema{
 		Parse: func(input any) error {
-			if input == nil || reflect.TypeOf(input).Kind() != reflect.Float64 {
-				errMsg := fmt.Sprintf("'%s' is not a number.", varName)
+			var floatInput float64
+			switch v := input.(type) {
+			case int:
+				floatInput = float64(v)
+			case float32:
+				floatInput = float64(v)
+			case float64:
+				floatInput = v
+			default:
+				errMsg := fmt.Sprintf("[%s]: '%v' is not a number.", varName, input)
 				return errors.New(errMsg)
 			}
+
+			if reflect.TypeOf(floatInput).Kind() != reflect.Float64 {
+				errMsg := fmt.Sprintf("[%s]: '%v' is not a number.", varName, input)
+				return errors.New(errMsg)
+			}
+
+			if math.IsNaN(floatInput) {
+				errMsg := fmt.Sprintf("[%s]: '%v' is NaN.", varName, input)
+				return errors.New(errMsg)
+			}
+
 			return nil
 		},
 	}
--- primitives_test.go
+++ primitives_test.go
@@ -1,6 +1,7 @@
 package meow
 
 import (
+	"math"
 	"testing"
 	"time"
 )
@@ -22,6 +23,32 @@ func TestStringInvalid(t *testing.T) {
 	schema := String("schema")
 
 	inputs := []any{nil, true, false, 123, time.Now()}
+
+	for _, in := range inputs {
+		err := schema.Parse(in)
+		if err == nil {
+			t.Errorf("For an invalid input '%s', expected an error, but got none instead.", in)
+		}
+	}
+}
+
+func TestNumberValid(t *testing.T) {
+	schema := Number("schema")
+
+	inputs := []any{1, -1, math.Inf(1), math.Inf(-1), 0.5, 0}
+
+	for _, in := range inputs {
+		err := schema.Parse(in)
+		if err != nil {
+			t.Errorf("For a valid input '%s', expected no error, but got %v", in, err)
+		}
+	}
+}
+
+func TestNumberInvalid(t *testing.T) {
+	schema := Number("schema")
+
+	inputs := []any{"Hello", nil, true, false}
 
 	for _, in := range inputs {
 		err := schema.Parse(in)
